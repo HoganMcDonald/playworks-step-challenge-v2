3075774eb96f822576edeedf4c6025fb6dff179f
Hogan McDonald <hogan.developer@gmail.com>
2020-12-20 01:41:40 -0600

    adds tables

---
+++ db/migrate/20201220071622_create_tables.rb
@@ -0,0 +1,60 @@
+class CreateTables < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contests do |t|
+      t.string :name
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+
+    create_table :teams do |t|
+      t.string :name
+      t.string :company_name
+      t.references :contests, foreign_key: true
+
+      t.timestamps
+    end
+
+    create_join_table :users, :teams do |t|
+      t.index :user_id
+      t.index :team_id
+    end
+
+    create_table :challenges do |t|
+      t.string :name
+      t.text :description
+      t.date :due_date
+      t.references :contests, foreign_key: true
+
+      t.timestamps
+    end
+
+    create_table :steps do |t|
+      t.integer :count
+      t.references :users, foreign_key: true
+
+      t.timestamps
+    end
+
+    create_table :content do |t|
+      t.string :content_type
+      t.text :text
+
+      t.timestamps
+    end
+
+    create_table :roles do |t|
+      t.string :name
+
+      t.timestamps
+    end
+
+    create_join_table :users, :roles do |t|
+      t.index :user_id
+      t.index :role_id
+    end
+
+    add_column :users, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_020433) do
+ActiveRecord::Schema.define(version: 2020_12_20_071622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "challenges", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "due_date"
+    t.bigint "contests_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contests_id"], name: "index_challenges_on_contests_id"
+  end
+
+  create_table "content", force: :cascade do |t|
+    t.string "content_type"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contests", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "role_id", null: false
+    t.index ["role_id"], name: "index_roles_users_on_role_id"
+    t.index ["user_id"], name: "index_roles_users_on_user_id"
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.integer "count"
+    t.bigint "users_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_steps_on_users_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "company_name"
+    t.bigint "contests_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contests_id"], name: "index_teams_on_contests_id"
+  end
+
+  create_table "teams_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "team_id", null: false
+    t.index ["team_id"], name: "index_teams_users_on_team_id"
+    t.index ["user_id"], name: "index_teams_users_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +85,12 @@ ActiveRecord::Schema.define(version: 2020_12_20_020433) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "challenges", "contests", column: "contests_id"
+  add_foreign_key "steps", "users", column: "users_id"
+  add_foreign_key "teams", "contests", column: "contests_id"
 end
