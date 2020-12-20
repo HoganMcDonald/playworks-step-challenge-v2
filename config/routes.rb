567d570c6847f5098ed662325b684680274a977f
Hogan McDonald <hogan.developer@gmail.com>
2020-12-19 20:01:11 -0600

    adds template for routing and favicon to layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # devise_for :users
+
+  get '/login', to: 'home#index'
+
+  # authenticated :user do
+  #   root to: 'home#index', as: :authenticated_root
+  #   # get '/example', to: 'home#index'
+  # end
+
+  root to: redirect('/login')
 end
