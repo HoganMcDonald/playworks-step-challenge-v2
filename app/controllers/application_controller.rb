1d295f952155df0800eec32ad2836e729d45beb5
Hogan McDonald <hogan.developer@gmail.com>
2020-12-20 18:31:58 -0600

    allows devise to respond to json without csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  respond_to :json
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to login_path, :notice => 'if you want to add a notice'
+    end
+  end
 end
