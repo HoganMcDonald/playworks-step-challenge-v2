a3ced5945b1f6568b37306ddd59a189607c8ff7e
Hogan McDonald <hogan.developer@gmail.com>
2020-12-20 23:55:04 -0600

    fixes validation error

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -2,17 +2,13 @@ class Contest < ApplicationRecord
   has_many :teams
 
   validates_presence_of :name, :start_date, :end_date
-  validates :dates
+  validate :dates
 
   private
 
   def dates
     if start_date > end_date
       errors.add :end_date, 'constest must end after it starts'
-    end
-
-    if start_date < DateTime.now
-      errors.add :start_date, 'constests must take place in the future'
     end
   end
 end
