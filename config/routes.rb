a8345d22dc768c9445e40d4826316d188fc5bcad
Hogan McDonald <hogan.developer@gmail.com>
2020-12-20 22:27:53 -0600

    creates custom reset password route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'home#index', as: :authenticated_root
 
   get '/login', to: 'home#index'
   get '/signup', to: 'home#index'
   get '/forgot-password', to: 'home#index'
+  get '/reset-password', to: 'home#index'
 end
